95248e07a6ff1de04a88901ce84d4d8889823db4
Terra Zero <terrazerocore@gmail.com>
2024-05-12 01:50:32 +0200

    up

--- src/Scaffold.js
+++ src/Scaffold.js
@@ -1,4 +1,23 @@
+const Path = require('path');
+const FS = require('fs');
+
 module.exports = class Scaffold {
+
+  /**
+   * @param {string} dir 
+   * @returns {string}
+   */
+  findPackage(dir) {
+    let last = null;
+    do {
+      last = dir;
+      dir = Path.join(dir, '..');
+      if (FS.existsSync(Path.join(dir, 'package.json'))) {
+        return Path.join(dir, 'package.json');
+      } 
+    } while (last !== dir);
+    return null;
+  }
 
   scaffold(config) {
     console.log(config);
